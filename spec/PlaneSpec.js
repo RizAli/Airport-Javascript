85283f18678139f8f38a5353eca0d22f1839cc41
Rizwan Ali <rizcube@gmail.com>
2015-03-03 17:36:54 +0000

    Plane created and landed

---
+++ spec/PlaneSpec.js
@@ -0,0 +1,16 @@
+describe('Plane', function() {
+  var plane;
+
+  it('should be in the air when create', function() {
+      plane = new Plane();
+      expect(plane.isFlying).toBe(true);
+  });
+
+  it('should be able to land', function(){
+      plane = new Plane();
+      plane.land();
+      expect(plane.isFlying).toBe(false);
+  });
+
+});
+
